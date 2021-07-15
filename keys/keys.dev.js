f06098b819ccefd19f06c4f66df154c7f3440c5a
Vadim <terrwin.official@gmail.com>
2021-07-15 16:21:50 +0300

    first commit

---
+++ keys/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI: `mongodb+srv://developer:yDShtMPBH5QKgJHp@cluster0.sixeu.mongodb.net/course`,
+    SESSION_SECRET:'some secret value',
+    SEND_GRID_API_KEY:'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',
+    subjectEmail:'Account has been created',
+    BASE_URL:'http://localhost:8080'
+}
